1488c515f488c0d0e8017619b78d01c2608fa8c4
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-02-08 17:29:27 -0300

    Adição de novo projeto Tarefas

---
+++ Tarefas_List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarefas_List
+//
+//  Created by Bruno Cassiano on 19/02/19.
+//  Copyright © 2019 Bruno Cassiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
